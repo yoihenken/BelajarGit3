034f5e8b74073cdec75dee9e036aeb44fe85d665
yoihenken <bagusbayu0310tkj1@gmai.com>
2019-09-29 10:44:03 +0700

    Mengubah tulisan

--- src/com/yoihenken/BelajarGit3/mainin.java
+++ src/com/yoihenken/BelajarGit3/mainin.java
@@ -2,6 +2,6 @@ package com.yoihenken.BelajarGit3;
 
 public class mainin {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git Hub");
     }
 }

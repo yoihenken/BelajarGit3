f9066be6ec3a28401bdbeac23628c9f978c35df2
yoihenken <bagusbayu0310tkj1@gmai.com>
2019-09-29 11:25:13 +0700

    menambah get set dan constract

--- src/com/yoihenken/BelajarGit3/Ningen.java
+++ src/com/yoihenken/BelajarGit3/Ningen.java
@@ -1,4 +1,37 @@
 package com.yoihenken.BelajarGit3;
 
 public class Ningen {
+    private int ummur;
+    private String nama;
+    private String jabatan;
+
+    public Ningen(int ummur, String nama, String jabatan) {
+        this.ummur = ummur;
+        this.nama = nama;
+        this.jabatan = jabatan;
+    }
+
+    public int getUmmur() {
+        return ummur;
+    }
+
+    public void setUmmur(int ummur) {
+        this.ummur = ummur;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJabatan() {
+        return jabatan;
+    }
+
+    public void setJabatan(String jabatan) {
+        this.jabatan = jabatan;
+    }
 }

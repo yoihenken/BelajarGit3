67840d933921551a7e1c37c784b4df119b0532b9
yoihenken <bagusbayu0310tkj1@gmai.com>
2019-09-29 10:56:59 +0700

    MEnambah class Neko dan bisa bicara

---
+++ src/com/yoihenken/BelajarGit3/neko.java
@@ -0,0 +1,11 @@
+package com.yoihenken.BelajarGit3;
+
+public class neko {
+    private static void talk(){
+        System.out.println("Myaaww");
+    }
+
+    public static void main(String[] args) {
+        talk();
+    }
+}
